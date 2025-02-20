1cb4c4da2b74711b276c07211a09c9db03f8c598
kimkim168 <KimKim168>
2025-02-21 00:03:08 +0700

    gitignore two

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        // hostname: "chhayhok.brolong.pro",
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "8000",
+         hostname: "supportal-alphaib.kimsoreya.site",
       },
     ],
   },
