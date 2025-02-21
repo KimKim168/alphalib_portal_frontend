689d7510516b000c4aa504504f829d0dca9d4a13
kimkim168 <KimKim168>
2025-02-21 12:47:16 +0700

    animetion

--- components/my-loading-animation.jsx
+++ components/my-loading-animation.jsx
@@ -4,7 +4,7 @@ export default function MyLoadingAnimation() {
     // <div className="flex flex-col items-center justify-center w-full">
     //  Loading...
     // </div>
-    <div className='flex space-x-2 justify-center items-center  dark:invert'>
+    <div className='flex space-x-2 justify-center items-center h-screen dark:invert'>
  	<span className='sr-only'>Loading...</span>
   <div className='h-8 w-8 bg-[#9b9dd9] rounded-full animate-bounce [animation-delay:-0.3s]'></div>
 	<div className='h-8 w-8 bg-[#635ca1] rounded-full animate-bounce [animation-delay:-0.15s]'></div>
